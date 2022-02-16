690b84363d195cf5248e7b505239fcd10349a91e
sukhjeetkaur123 <97154128+sukhjeetkaur123@users.noreply.github.com>
2022-02-16 21:53:57 +0530

    Add files via upload

---
+++ comJson_parsing/src/test/java/comJson_parsing_getSet/Person.java
@@ -0,0 +1,43 @@
+package comJson_parsing_getSet;
+
+
+
+public class Person {
+	private String email;
+	private String firstName;
+	private String lastName;
+	private Address address;
+	private String state;
+	public Address getAddress() {
+		return address;
+	}
+
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+	public Address getAdress()
+	{
+		return address;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+
+}
